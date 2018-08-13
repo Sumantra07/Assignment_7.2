ef0d42e5fcd0a78d109406430456ce7821d7c72c
Bhattacharya <sumantra.bhattacharya@siemens.com>
2018-08-14 00:45:00 +0530

    Assignment-7.2

---
+++ script.js
@@ -0,0 +1,23 @@
+// using for loop//
+console.log("using for loop...");
+for( var i = 0; i <= 50 ; i++)
+{   // modulus operator check if its a multiple of 5
+	if( i% 5 == 0)
+	{
+		console.log(i);
+	}
+	
+}
+console.log ("using while loop...");	
+//using while loop//
+var j = 0;
+while( j <= 50)
+{
+	// modulus operator check if its a multiple of 5
+	if( j% 5 == 0)
+	{
+		console.log(j);
+	}
+	j++;
+}
+	
